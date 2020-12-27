18f2be4df6b5ee7675e88935c4799034ca389d4c
hibaelechi@gmail.com <hibaelechi@gmail.com>
2020-12-27 19:57:24 +0100

    authen

--- ujob/src/app/login-cmp/login-cmp.component.ts
+++ ujob/src/app/login-cmp/login-cmp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login-cmp',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-cmp.component.scss'],
 })
 export class LoginCmpComponent implements OnInit {
-  constructor() {}
+  loginCompanyData = {
+    username: '',
+    password: '',
+  };
+  constructor(private _auth: AuthService) {}
 
   ngOnInit(): void {}
+  loginCompany() {
+    console.log(this.loginCompanyData);
+    this._auth.loginCompany(this.loginCompanyData).subscribe(
+      (res: any) => console.log(res),
+      (err: any) => console.log(err)
+    );
+  }
 }
--- ujob/src/app/signe-user/signe-user.component.ts
+++ ujob/src/app/signe-user/signe-user.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { HttpClient } from '@angular/common/http';
+
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signe-user',
   templateUrl: './signe-user.component.html',
-  styleUrls: ['./signe-user.component.scss']
+  styleUrls: ['./signe-user.component.scss'],
 })
 export class SigneUserComponent implements OnInit {
+  registerUserData = {
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    password: '',
+  };
+  constructor(private _auth: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  registerUser() {
+    console.log(this.registerUserData);
+    this._auth.registerUser(this.registerUserData).subscribe(
+      (res: any) => console.log(res),
+      (err: any) => console.log(err)
+    );
   }
-
 }
