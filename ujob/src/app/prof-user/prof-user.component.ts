3b5cc65cc13f9603020d5b651f129c53a89f1ac7
marwen <marwenkhorchani7@gmail.com>
2020-12-28 23:27:06 +0100

    apdate profile done

--- ujob/src/app/prof-user/prof-user.component.ts
+++ ujob/src/app/prof-user/prof-user.component.ts
@@ -1,5 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../services/user.service.service';
 @Component({
   selector: 'app-prof-user',
   templateUrl: './prof-user.component.html',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfUserComponent implements OnInit {
   user = JSON.parse(localStorage.getItem('user') || '{}');
-  constructor() {}
+  constructor(private _http: HttpClient, private userService: UserService) {}
 
-  ngOnInit(): void {}
-
-  
+  ngOnInit(): void {
+    this.getUserById();
+  }
+  getUserById() {
+    var id = this.user._id;
+    this.userService.myNewProf(id).subscribe((response) => {
+      this.user = response;
+    });
+  }
 }
