63bf4ba4519a08d338cff3cc7a3a9f0272f72d07
marwen <marwenkhorchani7@gmail.com>
2020-12-29 18:02:42 +0100

    testttt

--- ujob/src/app/edit-profile-user/edit-profile-user.component.ts
+++ ujob/src/app/edit-profile-user/edit-profile-user.component.ts
@@ -2,7 +2,7 @@ import { importExpr } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { EditProfUserService } from '../edit-prof-user.service';
 import { NgForm } from '@angular/forms';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-edit-profile-user',
   templateUrl: './edit-profile-user.component.html',
@@ -52,7 +52,8 @@ export class EditProfileUserComponent implements OnInit {
     skill2: '',
     skill3: '',
   };
-  constructor(private edit: EditProfUserService) {}
+  
+  constructor(private edit: EditProfUserService,private router: Router) {}
 
   ngOnInit(): void {
     // this..subscribe(user=>{
@@ -64,12 +65,14 @@ export class EditProfileUserComponent implements OnInit {
   editProf(form: NgForm) {
     // console.log(form);
     var obj = form.value;
-    // console.log(obj);
+    console.log(obj);
     var id = this.user._id;
 
     this.edit.editProf(id, obj).subscribe(
       (res: any) => console.log(res)
       // (err: any) => console.log(err)
+
     );
+    this.router.navigate(['/myprofile-user'])
   }
 }
